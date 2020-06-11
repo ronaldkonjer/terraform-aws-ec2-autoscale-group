65779b700cd2012f22e0f748d279cb42451690c4
Ronald Konjer <ronaldkonjer@gmail.com>
2020-06-11 09:13:08 +0200

    add launch configuration as output

--- outputs.tf
+++ outputs.tf
@@ -52,3 +52,9 @@ output "autoscaling_group_health_check_type" {
   description = "`EC2` or `ELB`. Controls how health checking is done"
   value       = join("", aws_autoscaling_group.default.*.health_check_type)
 }
+
+output "autoscaling_group_launch_configuration" {
+  description = "`Launch configuration used to provision the instances"
+  value       = join("", aws_autoscaling_group.default.*.launch_configuration)
+}
+
